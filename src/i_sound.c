bbd568b6ce25294953446df36c0a09ed46da252c
Fabillotic <fabillo552@gmail.com>
2022-12-28 19:18:45 +0100

    Add file headers and copyright notices

--- src/i_sound.c
+++ src/i_sound.c
@@ -1,3 +1,23 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) 2022 by Fabillotic
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//
+// DESCRIPTION:
+//	System interface for sound.
+//
+//-----------------------------------------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -270,6 +290,7 @@ int I_QrySongPlaying(int handle) {
 }
 
 //
+// Copyright (C) 1993-1996 by id Software, Inc.
 // This function loads the sound data from the WAD lump,
 //  for single sound.
 //
--- src/i_video.c
+++ src/i_video.c
@@ -1,3 +1,23 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) 2022 by Fabillotic
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//
+// DESCRIPTION:
+//	DOOM graphics stuff for X11, UNIX.
+//
+//-----------------------------------------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -157,6 +177,9 @@ void I_StartTic() {
 void I_StartFrame() {
 }
 
+//
+// Copyright (C) 1993-1996 by id Software, Inc.
+//
 int xlatekey(KeySym sym) {
 	int rc;
 
